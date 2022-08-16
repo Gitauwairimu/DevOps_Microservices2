03bd2021630b6dec19008d0a1502d3d4a7ec23a2
gitauwairimu <gitauwairimu@gmail.com>
2022-08-17 00:04:13 +0300

    Edit name

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -9,14 +9,16 @@ dockerpath="gitauwairimu/ml-microservice:v1.0.1"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run predictApp\
-    --image=$dockerpath\
-    --port=80 --labels app=ml-predictApp
 
+# kubectl run douh\
+#     --image=$dockerpath\
+#     --port=80 --labels app=ml-predictApp
+
+kubectl run douh --image=gitauwairimu/ml-microsrevice:v1.0.1 --port=80
 # Step 3:
 # List kubernetes pods
 kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-kubectl port-forward predictApp 8000:80
+kubectl port-forward douh 8000:80
