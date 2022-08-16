892727e79be7b4950542bd14c2e3a63277bb6f1a
gitauwairimu <gitauwairimu@gmail.com>
2022-08-16 17:49:25 +0300

    Edit k8s.sh & run_docker scripts

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,18 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="gitauwairimu/predict"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run predictApp\
+    --image=$dockerpath\
+    --port=80 --labels app=predictApp
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward predictApp 8000:80
